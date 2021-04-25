1d9bd11560c570130e05f9b3508302dafea0d082
pickettmap <mipi6515@colorado.edu>
2021-04-25 15:18:36 -0600

    basic leaderboard functionality

--- src/app/components/ui/leaderboardentry/leaderboardentry.component.ts
+++ src/app/components/ui/leaderboardentry/leaderboardentry.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Angler } from 'src/app/models/angler';
 import { Catch } from 'src/app/models/catch';
+import { Fish } from 'src/app/models/fish';
 import { fishDetails } from 'src/app/models/fishDetails';
 import { CatchService } from 'src/app/services/catch.service';
 import { FishService } from 'src/app/services/fish.service';
@@ -12,20 +13,33 @@ import { FishService } from 'src/app/services/fish.service';
 })
 export class LeaderboardentryComponent implements OnInit {
 
-  @Input() angler!:Angler
+  @Input() angler!:any
   catch!: Catch;
-  fish!: fishDetails;
+  fish!: Fish;
 
   constructor(private fishService : FishService,private catchService : CatchService) { }
 
-  getFishInfo() {
-    catch = this.catchService.getAll()
-    fish = this.fishService.getFish(catch.fishID)
-    }
-
   ngOnInit(): void {
+    this.setFish()
+    // console.log(this.fish)
   }
 
+  setFish() {
+    if(this.angler.catches.length > 0) {
+      for(let catchy of this.angler.catches) {
+          console.log(catchy)
+      }
+      this.findMaxFish()
+    }
 
+  }
 
+  findMaxFish() {
+    let max:Number = 0;
+    for(let catchy of this.angler.catches) {
+      if(catchy.fish.length > max) {
+        this.fish = catchy.fish;
+      }
+    }
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://project2fisapp-env.eba-rpk7wuxb.us-west-1.elasticbeanstalk.com/leaderboard';
+const baseUrl = 'http://project2fisapp-env.eba-rpk7wuxb.us-west-1.elasticbeanstalk.com/angler';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class UserService {
 
   constructor(private http:  HttpClient) { }
 
-  getAnglers(): Observable<any> {
-    return this.http.get<any>(baseUrl);
+  getAnglers(): Observable<any[]> {
+    return this.http.get<any[]>(baseUrl);
   }
 }

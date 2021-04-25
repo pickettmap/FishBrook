0c896b1c76b41c9b809c45f68fb735c56e6d1c41
Nshaji24 <66273445+Nshaji24@users.noreply.github.com>
2021-04-25 16:00:05 -0400

    Update leaderboardentry.component.ts

--- src/app/components/ui/leaderboardentry/leaderboardentry.component.ts
+++ src/app/components/ui/leaderboardentry/leaderboardentry.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Angler } from 'src/app/models/angler';
 import { Catch } from 'src/app/models/catch';
 import { fishDetails } from 'src/app/models/fishDetails';
+import { CatchService } from 'src/app/services/catch.service';
+import { FishService } from 'src/app/services/fish.service';
 
 @Component({
   selector: 'app-leaderboardentry',
@@ -14,9 +16,16 @@ export class LeaderboardentryComponent implements OnInit {
   catch!: Catch;
   fish!: fishDetails;
 
-  constructor() { }
+  constructor(private fishService : FishService,private catchService : CatchService) { }
+
+  getFishInfo() {
+    catch = this.catchService.getAll()
+    fish = this.fishService.getFish(catch.fishID)
+    }
 
   ngOnInit(): void {
   }
+
+
 
 }

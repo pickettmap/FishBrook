37a976e0f759c1305ec4719f1240813d077579b9
Nshaji24 <66273445+Nshaji24@users.noreply.github.com>
2021-04-25 15:59:49 -0400

    Update leaderboardlist.component.ts

--- src/app/components/ui/leaderboardlist/leaderboardlist.component.ts
+++ src/app/components/ui/leaderboardlist/leaderboardlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Angler } from 'src/app/models/angler';
+import {UserService} from 'src/app/services/user.service'
 
 @Component({
   selector: 'app-leaderboardlist',
@@ -8,10 +9,33 @@ import { Angler } from 'src/app/models/angler';
 })
 export class LeaderboardlistComponent implements OnInit {
 
+  page = 1;
+  count = 0;
+  pageSize = 3;
   leaderboardUsers: Angler[] = [];
-  constructor() { }
+
+  constructor(private userService : UserService) { }
 
   ngOnInit(): void {
+    this.getAnglers
+  }
+
+  getAnglers() : void{
+    this.userService.getAnglers()
+    .subscribe(
+      (response) => {this.leaderboardUsers.push(response)})
+  }
+
+  
+  handlePageChange(event: number): void {
+    this.page = event;
+    this.getAnglers();
+  }
+
+  handlePageSizeChange(event: any): void {
+    this.pageSize = event.target.value;
+    this.page = 1;
+    this.getAnglers();
   }
 
 }
